a013fdb9ac916ae823d3be3cd37dd596bf92538f
Dtminer-kl <71681337+Dtminer-kl@users.noreply.github.com>
2024-07-21 14:54:43 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class InvestmentFund:
     def __init__(self, fund_id, fund_name, fund_manager_name, fund_description, fund_nav, fund_creation_date, fund_performance):
         self.fund_id = fund_id
@@ -7,5 +9,15 @@ class InvestmentFund:
         self.fund_nav = fund_nav
         self.fund_creation_date = fund_creation_date
         self.fund_performance = fund_performance
-
+        
+def to_dict(self):
+        return {
+            'fund_id': self.id,
+            'fund_name': self.fund_name,
+            'fund_manager_name': self.fund_manager_name,
+            'fund_description': self.fund_description,
+            'fund_nav': self.fund_nav,
+            'fund_creation_date': self.fund_creation_date.strftime('%Y-%m-%d'),
+            'fund_performance': self.fund_performance
+        }
   

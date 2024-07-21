e0f25b7df65b638c9b877a0c24b5e94862429638
Dtminer-kl <71681337+Dtminer-kl@users.noreply.github.com>
2024-07-21 14:52:55 +0800

    Create models.py
    made InvestmentFund for model design

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class InvestmentFund:
+    def __init__(self, fund_id, fund_name, fund_manager_name, fund_description, fund_nav, fund_creation_date, fund_performance):
+        self.fund_id = fund_id
+        self.fund_name = fund_name
+        self.fund_manager_name = fund_manager_name
+        self.fund_description = fund_description
+        self.fund_nav = fund_nav
+        self.fund_creation_date = fund_creation_date
+        self.fund_performance = fund_performance
+
+  
